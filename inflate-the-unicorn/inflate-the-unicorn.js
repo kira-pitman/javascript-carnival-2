04cd2b54b1e2d93e1d3bb2b7d6a9723a4c698fbf
Kira Pitman <kirapitman@gmail.com>
2024-03-07 10:32:48 +1300

    make 1 unicorn inflate

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -1,5 +1,27 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Inflate The Unicorn!")
+console.log('Inflate The Unicorn!')
+
+let unicorns = document.getElementsByClassName('unicorn')
+
+for (let unicorn of unicorns) {
+  unicorn.onclick = unicornClicked
+}
+//put in array afterwards
+let unicorn0 = 0
+let unicorn1 = 0
+let unicorn2 = 0
+
+function unicornClicked(e) {
+  let unicorn = e.currentTarget
+  let id = unicorn.id
+
+  if (id == 0) {
+    unicorn0 += 1 // increase unicorn 0 number
+    unicorn.src = `./images/unicorn-${unicorn0}.png`
+  }
+
+  console.log('clicked ur unicorn')
+}
